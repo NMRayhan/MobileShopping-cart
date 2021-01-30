d2c754e1fd23a664c72576a1424375a3b0644030
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-31 04:27:11 +0600

    function Modification

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
+function handleProductChange(){
+    const mobileInput = document.getElementById("mobile-Count");
+    const mobileCount = parseInt(mobileInput.value);
+    var totalMobile = decrement(mobileCount);
+    mobileInput.value = totalMobile;
+    const totalMobilePrice = totalMobile * 1219;
+    document.getElementById("mobilePrice").innerText = totalMobilePrice;
+ }
+
+ function decrement(mobileCount){
+    const totalMobile = mobileCount - 1;
+    return totalMobile;
+ }
+
+ function increment(mobileCount){
+    const totalMobile = mobileCount + 1;
+    return totalMobile;
+
+ }
 document.getElementById("mobile-Plus").addEventListener("click", function () {
     const mobileInput = document.getElementById("mobile-Count");
     const mobileCount = parseInt(mobileInput.value)
-    const totalMobile = mobileCount + 1;
+    const totalMobile = increment(mobileCount);
     mobileInput.value = totalMobile;
     const totalMobilePrice = totalMobile * 1219;
     document.getElementById("mobilePrice").innerText = totalMobilePrice;
  })
 
  document.getElementById("mobile-Minus").addEventListener("click", function (event) {
-    const mobileInput = document.getElementById("mobile-Count");
-    const mobileCount = parseInt(mobileInput.value);
-    const totalMobile = mobileCount - 1;
-    mobileInput.value = totalMobile;
-    const totalMobilePrice = totalMobile * 1219;
-    document.getElementById("mobilePrice").innerText = totalMobilePrice;
+    handleProductChange()
  })
+
